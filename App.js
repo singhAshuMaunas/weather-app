95000e914d2d967302a7f6db46294ae1bd463951
singhAshuMaunas <102754668+singhAshuMaunas@users.noreply.github.com>
2022-09-12 15:08:18 +0530

    Initial commit

---
+++ App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+import SearchMain from "./src/components/searchMain.js";
+
+function App() {
+  return (
+    <div className="App">
+      <SearchMain />
+    </div>
+  );
+}
+
+export default App;
